aeb36da84ed17ca49ae6aa646533f5a2393c1216
Daniel Cunha <danielcunha54@gmail.com>
2020-06-25 21:23:33 -0300

    fix: Fixed LoginView overflow on landscape mode

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -9,48 +9,44 @@ class LoginView extends StatelessWidget {
     final mediaQuery = MediaQuery.of(context);
 
     return Scaffold(
-      body: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxHeight: mediaQuery.size.height),
-          child: Padding(
-            padding: EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-              top: mediaQuery.viewPadding.top,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const Spacer(flex: 2),
-                const Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  height: 200,
-                ),
-                const Spacer(flex: 1),
-                LoginForm(),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: DefaultFlatButton(
-                    onPressed: () {},
-                    content: 'Forgot your password?',
+      body: LayoutBuilder(
+        builder: (ctx, cons) => SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minHeight: cons.maxHeight),
+            child: IntrinsicHeight(
+              child: Column(
+                children: <Widget>[
+                  const Spacer(flex: 2),
+                  SizedBox(height: mediaQuery.viewPadding.top),
+                  const Image(
+                    image: AssetImage('assets/images/logo.png'),
+                    height: 200,
                   ),
-                ),
-                const Spacer(flex: 4),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text("Doesn't have an account?"),
-                    DefaultFlatButton(
+                  const Spacer(flex: 1),
+                  LoginForm(),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: DefaultFlatButton(
                       onPressed: () {},
-                      content: 'Register',
-                      color: theme.primaryColor,
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      content: 'Forgot your password?',
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const Spacer(flex: 4),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text("Doesn't have an account?"),
+                      DefaultFlatButton(
+                        onPressed: () {},
+                        content: 'Register',
+                        color: theme.primaryColor,
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
